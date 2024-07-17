79e6f15e87f11a3b69f17b757df4c58729b41f65
GabrielCFormiga <gabriel.campfor@gmail.com>
2024-07-17 16:33:47 -0300

    Geração de novos resultados

--- sortings/src/insertion.c
+++ sortings/src/insertion.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+#define MAX_PATH_LEN 256
 
 // Insertion Sort
 
@@ -28,43 +32,69 @@ void insertionSort(int *ar, int n) {
 }
 
 int main() {
+    DIR *dir;
+    struct dirent *entry;
+    const char *intancesFolder = "/home/campfor/Área de Trabalho/Estrutura de Dados I/sortings/instancias-num/";
+    const char *resultsFolder = "/home/campfor/Área de Trabalho/Estrutura de Dados I/sortings/resultados-num/insertion/";
+
     FILE *inputFile, *outputFile;
+    char inputPath[MAX_PATH_LEN], outputPath[MAX_PATH_LEN];
+
     clock_t start, end;
     double cpu_time_used;
-    int ar[100000];
-    int x, i = 0;
 
-    inputFile = fopen("/home/campfor/Área de Trabalho/Estrutura de Dados I/sortings/instancias-num/num.100000.1.in", "r");
-    if (inputFile == NULL) {
-        perror("Erro ao abrir o arquivo de entrada");
+    dir = opendir(intancesFolder);
+    if (dir == NULL) {
+        perror("Erro ao abrir o diretório");
         return EXIT_FAILURE;
     }
 
-    outputFile = fopen("/home/campfor/Área de Trabalho/Estrutura de Dados I/sortings/resultados-num/insertion/num.100000.1.out", "w");
-    if (outputFile == NULL) {
-        perror("Erro ao abrir o arquivo de saída");
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        int ar[100000];
+        int x, i = 0;
+
+        // constrói o caminho completo do arquivo
+        snprintf(inputPath, MAX_PATH_LEN, "%s/%s", intancesFolder, entry->d_name);
+        snprintf(outputPath, MAX_PATH_LEN, "%s/%st", resultsFolder, entry->d_name);
+        outputPath[strlen(outputPath) - 3] = 'o';
+        outputPath[strlen(outputPath) - 2] = 'u';
+
+        inputFile = fopen(inputPath, "r");
+        if (inputFile == NULL) {
+            perror("Erro ao abrir o arquivo de entrada");
+            return EXIT_FAILURE;
+        }
+
+        outputFile = fopen(outputPath, "w");
+        if (outputFile == NULL) {
+            perror("Erro ao abrir o arquivo de saída");
+            fclose(inputFile);
+            return EXIT_FAILURE;
+        }
+
+        while (fscanf(inputFile, "%d", &x) != EOF) {
+            ar[i] = x;
+            i++;
+        }
+
+        start = clock();
+        insertionSort(ar, i);
+        end = clock();
+
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        fprintf(outputFile, "Insertion Sort:\n");
+        fprintf(outputFile, "Tempo de CPU usado: %f segundos\n", cpu_time_used);
+        for (int j = 0; j < i; j++) {
+            fprintf(outputFile, "%d\n", ar[j]);
+        }
+
         fclose(inputFile);
-        return EXIT_FAILURE;
+        fclose(outputFile);
     }
-
-    while (fscanf(inputFile, "%d", &x) != EOF) {
-        ar[i] = x;
-        i++;
-    }
-
-    start = clock();
-    insertionSort(ar, i);
-    end = clock();
-
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    fprintf(outputFile, "Insertion Sort:\n");
-    fprintf(outputFile, "Tempo de CPU usado: %f segundos\n", cpu_time_used);
-    for (int j = 0; j < i; j++) {
-        fprintf(outputFile, "%d\n", ar[j]);
-    }
-
-    fclose(inputFile);
-    fclose(outputFile);
 
     return 0;
 }
--- sortings/src/quick.c
+++ sortings/src/quick.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+#define MAX_PATH_LEN 256
+
 // Quick Sort
 
 // Time Complexity:
@@ -38,4 +46,72 @@ void quickSort(int *ar, int l, int r) {
     int i = particao(ar, l, r);
     quickSort(ar, l, i - 1);
     quickSort(ar, i + 1, r);
+}
+
+int main() {
+    DIR *dir;
+    struct dirent *entry;
+    const char *intancesFolder = "/home/campfor/Área de Trabalho/Estrutura de Dados I/sortings/instancias-num/";
+    const char *resultsFolder = "/home/campfor/Área de Trabalho/Estrutura de Dados I/sortings/resultados-num/quick/";
+
+    FILE *inputFile, *outputFile;
+    char inputPath[MAX_PATH_LEN], outputPath[MAX_PATH_LEN];
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    dir = opendir(intancesFolder);
+    if (dir == NULL) {
+        perror("Erro ao abrir o diretório");
+        return EXIT_FAILURE;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        int ar[100000];
+        int x, i = 0;
+
+        // constrói o caminho completo do arquivo
+        snprintf(inputPath, MAX_PATH_LEN, "%s/%s", intancesFolder, entry->d_name);
+        snprintf(outputPath, MAX_PATH_LEN, "%s/%st", resultsFolder, entry->d_name);
+        outputPath[strlen(outputPath) - 3] = 'o';
+        outputPath[strlen(outputPath) - 2] = 'u';
+
+        inputFile = fopen(inputPath, "r");
+        if (inputFile == NULL) {
+            perror("Erro ao abrir o arquivo de entrada");
+            return EXIT_FAILURE;
+        }
+
+        outputFile = fopen(outputPath, "w");
+        if (outputFile == NULL) {
+            perror("Erro ao abrir o arquivo de saída");
+            fclose(inputFile);
+            return EXIT_FAILURE;
+        }
+
+        while (fscanf(inputFile, "%d", &x) != EOF) {
+            ar[i] = x;
+            i++;
+        }
+
+        start = clock();
+        quickSort(ar, 0, i - 1);
+        end = clock();
+
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        fprintf(outputFile, "Quick Sort:\n");
+        fprintf(outputFile, "Tempo de CPU usado: %f segundos\n", cpu_time_used);
+        for (int j = 0; j < i; j++) {
+            fprintf(outputFile, "%d\n", ar[j]);
+        }
+
+        fclose(inputFile);
+        fclose(outputFile);
+    }
+
+    return 0;
 }
